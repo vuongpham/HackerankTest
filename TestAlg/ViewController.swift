9615f40c74a096e626f57b1084819a54f306b5b3
Vuongpt <vuongpt@runsystem.net>
2020-02-06 17:47:20 +0700

    Initial Commit

---
+++ TestAlg/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAlg
+//
+//  Created by Vuong on 2/6/20.
+//  Copyright © 2020 Viet Tuan Dinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
