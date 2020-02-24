d63a0e2ba34174c78f7dd730689fd7a04d98bf9b
Vuongpt <vuongpt@runsystem.net>
2020-02-24 09:47:27 +0700

    no message

--- TestAlg/ViewController.swift
+++ TestAlg/ViewController.swift
@@ -12,36 +12,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //print(bigSorting(unsorted: ["11111112398234820348230420340","3","4","5","2398234820348230420340"]))
         
         //let resultString = supperReduceString1(s: "baabcd")
-        let resultString = insertSort(s: [2,3,1])
-        
-        print(resultString)
+        //let resultString = insertSort(s: [2,3,1])
+          //  let result = binarySearchByiteratively(s: [1,3,4,5,8,10], x: 2)
+        // let result = binarySearchByRecursively(s: [1,3,4,5,8,10], x: 2, left: 0, right: 6 - 1)
+        let result = interpolationSearchByiteratively(s: [1,3,4,5,8,10], key: 10)
+        print(result)
     }
-    
-    func bigSorting(unsorted: [String]) -> [String] {
-        let  result = unsorted
-        quickSort(arr: result, low: 0, hight: result.count - 1)
-        return result
-    }
-    
-    func quickSort(arr: [String], low: Int, hight: Int) {
-        if low < hight {
-            
-        }
-    }
-    
-    func findPivotIndex(arr: [String], low: Int, high: Int) -> Int{
-        
-        let pivot = arr[high];
-        let i = low - 1
-        
-        
-        return 0
-    }
-    
-    
+
 // https://www.hackerrank.com/challenges/reduced-string/problem
     func supperReduceString(s: String) -> String {
         var result = ""
@@ -104,24 +83,88 @@ extension ViewController {
     func insertSort(s: [Int]) -> [Int] {
         
         var result = s
-        var i: Int = 0
+        let sizeS = s.count
         
-        while i < result.count - 1 {
-            let value = result[i]
-            var nextIndex = i + 1
-            if value > result[nextIndex]{
-                
-                while nextIndex < i {
-                    result[nextIndex] = result[nextIndex - 1]
-                    nextIndex = nextIndex - 1
-                }
+        for i in 1..<sizeS {
+            let t = result[i]
+            var preI = i - 1
+            
+            // If preValue bigger than value, i would move the value to after indet it bigger than.
+            while preI >= 0 && result[preI] > t {
+                result[preI+1] = result[preI]
+                preI = preI - 1
             }
             
-            i = i + 1
-
+            // Insert
+            result[preI + 1] = t
         }
         
         return result
     }
 }
 
+extension ViewController {
+    // iteratively
+    func binarySearchByiteratively(s: [Int], x: Int) -> Int? {
+        var leftIndex = 0
+        var rightIndex = s.count - 1
+        
+        while leftIndex <= rightIndex {
+            let midIndex = (rightIndex + leftIndex ) / 2
+            
+            if x == s[midIndex] {
+                return midIndex
+            } else if x < s[midIndex] {
+                rightIndex = midIndex - 1
+            } else if x > s[midIndex] {
+                leftIndex = midIndex + 1
+            }
+        }
+        
+        return nil
+    }
+    
+    
+    func binarySearchByRecursively(s: [Int], x: Int, left: Int, right: Int) -> Int? {
+        if left > right {
+            return nil
+        }
+        
+        let mid = (right + left) / 2  // Other way adjust mid index: left + ((right - left )/2)
+        
+        if s[mid] == x {
+            return mid
+        } else if s[mid] > x {
+            return binarySearchByRecursively(s: s, x: x, left: left, right: mid - 1)
+        } else if s[mid] < x {
+            return binarySearchByRecursively(s: s, x: x, left: mid + 1, right: right)
+        }
+        
+        return nil
+    }
+}
+
+extension ViewController {
+    
+    // Interpolation Search is improve binary search via mid = left + ((key - s[left]) * (right - left) / (s[right] - s[left]));
+    //
+    func interpolationSearchByiteratively(s: [Int], key: Int) -> Int? {
+        var left = 0
+        var right = s.count - 1
+        
+        while left <= right {
+            let mid = left + ((key - s[left]) * (right - left) / (s[right] - s[left]));
+            
+            if key == s[mid] {
+                return mid
+            } else if key < s[mid] {
+                right = mid - 1
+            } else if key > s[mid] {
+                left = mid + 1
+            }
+        }
+        
+        return nil
+    }
+
+}
